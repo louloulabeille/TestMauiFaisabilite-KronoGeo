f6e5cd8d0e69a1b01a8dc32ca112645bea5604d8
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-05-28 15:23:35 +0200

    Mise en place d'une API dans le projet pour faire des test au niveau du role et mise en place token JWT-Bearer

---
+++ Infracstructure/DataBase/TestContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infracstructure.DataBase
+{
+    public class TestContextFactory : IDesignTimeDbContextFactory<TestDbContext>
+    {
+        public TestDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TestDbContext>();
+            optionsBuilder.UseSqlite("C:\\Users\\loulo\\Desktop\\KronoGeo\\TestMauiFaisabilite-KronoGeo\\Infracstructure\\Data\\TestIdentity.db;");
+            return new TestDbContext(optionsBuilder.Options);
+        }
+    
+    }
+}
---
+++ Infracstructure/DataBase/TestDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infracstructure.DataBase
+{
+    public class TestDbContext : IdentityDbContext
+    {
+        #region Constructeur
+        public TestDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected TestDbContext()
+        {
+        }
+        #endregion
+
+        #region override method
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\loulo\\Desktop\\KronoGeo\\TestMauiFaisabilite-KronoGeo\\Infracstructure\\Data\\TestIdentity.db;");
+            //base.OnConfiguring(optionsBuilder);
+        }
+        #endregion
+
+    }
+}
